b281d689c28dcb3bca96b336dc59ed2983e0e472
Dimash Bexultanov <94544861+buzaubas@users.noreply.github.com>
2021-12-13 21:34:45 +0600

    Update !!!!!!!!!!!!!!!!!.cpp

--- !!!!!!!!!!!!!!!!!.cpp
+++ !!!!!!!!!!!!!!!!!.cpp
@@ -2,19 +2,20 @@
 #include <iostream>
 
 
-int t(double tempreture = 0) {
+int getdata(double tempreture = 0) {
     srand(time(NULL));
+    tempreture = rand() % 101;
     for (int i = 0; i < 4; i++) {
-        tempreture = rand() % 101;
         if (tempreture <= 50) {
             for (int i = 0; i < 4; i++) {
-                tempreture = rand() % 101;
                 if (tempreture <= 25) {
                     return tempreture;
                 }
+                tempreture = rand() % 101;
             }
             return tempreture;
         }
+        tempreture = rand() % 101;
     }
     return tempreture;
 }
