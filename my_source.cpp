6d4186d70ddc1a606d79a0866cb2460082b94fb6
Dimash Bexultanov <94544861+buzaubas@users.noreply.github.com>
2021-12-13 21:37:22 +0600

    Create my_source.cpp

---
+++ my_source.cpp
@@ -0,0 +1,17 @@
+int getdata(double tempreture = 0) {
+    srand(time(NULL));
+    tempreture = rand() % 101;
+    for (int i = 0; i < 4; i++) {
+        if (tempreture <= 50) {
+            for (int i = 0; i < 4; i++) {
+                if (tempreture <= 25) {
+                    return tempreture;
+                }
+                tempreture = rand() % 101;
+            }
+            return tempreture;
+        }
+        tempreture = rand() % 101;
+    }
+    return tempreture;
+}
